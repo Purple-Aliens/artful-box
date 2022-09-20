da1cb799dd1fae0110b85578c0442c403c29bbf7
jiyuuki <salma.hajrawi@gmail.com>
2022-09-20 18:23:09 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+import './index.css'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.use(ElementPlus)
+
+app.mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, js, vue}", 
+    "./src/**/*"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
